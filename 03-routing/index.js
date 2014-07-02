fbe35be5b9df649eaefdd27834911d50df31409d
Spencer Zhang <sepmein@gmail.com>
2014-07-02 21:53:41 +0800

    finished 03-routing

--- 03-routing/index.js
+++ 03-routing/index.js
@@ -3,14 +3,20 @@ var koa = require('koa');
 
 var app = module.exports = koa();
 
-app.use(function* () {
+app.use(function* (next) {
+	if(this.request.path !== '/') return yield next;
 
+	this.body = 'hello world';
 });
 
-app.use(function* () {
+app.use(function* (next) {
+	if(this.request.path !== '/404') return yield next;
 
+	this.body = 'page not found';
 });
 
-app.use(function* () {
+app.use(function* (next) {
+	if(this.request.path !== '/500') return yield next;
 
+	this.body = 'internal server error';
 });
