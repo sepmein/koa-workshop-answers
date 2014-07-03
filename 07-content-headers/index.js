839ceea09290b10207656b862dee154c42344eae
Spencer Zhang <sepmein@gmail.com>
2014-07-03 11:24:35 +0800

    finished 07-content-headers

--- 07-content-headers/index.js
+++ 07-content-headers/index.js
@@ -1,8 +1,16 @@
-
 var koa = require('koa');
 
 var app = module.exports = koa();
 
-app.use(function* () {
-  
+app.use(function * () {
+	if (this.request.is('application/json')) {
+		
+		//response type is implicitly setted by koa
+		this.body = {
+			message: 'hi!'
+		};
+	} else {
+		this.response.type = 'text/plain';
+		this.body = 'ok';
+	}
 })
